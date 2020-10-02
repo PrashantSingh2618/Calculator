100a997c46fd781e911b090d637111c9fd902dc1
PrashantSingh2618 <38156926+PrashantSingh2618@users.noreply.github.com>
2020-10-02 06:42:45 +0530

    Delete main.js

--- src/main.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue' ;
-import App from './App.vue' ;
-import CalculatorFunctions from './components/CalculatorFunctions.vue' ;
-
-
-const app = createApp(App);
-app.component('calc-functions',CalculatorFunctions) ;
-app.mount('#app')
